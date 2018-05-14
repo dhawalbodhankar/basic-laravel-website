16ef377fe1f40a5856fe47fb6cb7eed5d760311a
SendiSteve <sendi.stev@gmail.com.com>
2018-05-14 15:28:39 +0300

    Add styles for footer section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,5 +24,8 @@
             </div>
         </div>
     </div>
+    <footer id="footer" class="text-center">
+        <p>copyright 2018 &copy; Developed by Stephen</p>
+    </footer>
 </body>
 </html>
