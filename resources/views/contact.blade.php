349174acbbdd2f12054c80220e1e18682491e5c9
SendiSteve <sendi.stev@gmail.com.com>
2018-05-14 15:21:02 +0300

    Set up project structure

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Lets Talk</h1>
+    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Basic Laravel website</title>
+    {{-- css --}}
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    @include('includes.navbar')
+
+    <div class="container">
+        @if(Request::is('/'))
+            @include('includes.showcase')
+        @endif
+        <div class="row">
+            <div class="col-lg-8 col-md-8">
+                    @yield('content')
+            </div>
+            <div class="col-lg-4 col-md-4">
+                    @include('includes.sidebar')
+            </div>
+        </div>
+    </div>
+</body>
+</html>
